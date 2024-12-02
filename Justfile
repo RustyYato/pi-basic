c2f120585a0a590e84feb2116d4d5984325eaaa6
RustyYato <rustyyato@gmail.com>
2024-12-02 14:26:21 -0700

    setup ssh based file transfer to PI

--- Justfile
+++ Justfile
@@ -1,9 +1,9 @@
+set dotenv-load
+
 build:
-    CARGO_TARGET='aarch64-unknown-linux-gnu' CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER='aarch64-none-linux-gnu-gcc' cargo b -r
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER='aarch64-none-linux-gnu-gcc' cargo b -r --target aarch64-unknown-linux-gnu
 
-push:
-    ./scripts/check-uncommited.sh
-    git push
+send-unchecked: build
+    scp target/aarch64-unknown-linux-gnu/release/basic $IP:/home/yato/pi-basic/basic
 
-release name: build push
-    gh release -d {{name}} create target/aarch64-unknown-linux-gnu/release/basic
+send: build send-unchecked
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() -> Result<(), rppal::gpio::Error> {
 
     let mut led = pin2.into_output_high();
 
+    for _ in 0..10 {
+        led.set_low();
+        thread::sleep(time::Duration::from_millis(50));
+        led.set_high();
+        thread::sleep(time::Duration::from_millis(200));
+    }
+
     led.set_low();
-    thread::sleep(time::Duration::from_secs(1));
-    led.set_high();
-    thread::sleep(time::Duration::from_secs(1));
-    led.set_low();
-    thread::sleep(time::Duration::from_secs(1));
-    led.set_high();
-    thread::sleep(time::Duration::from_secs(1));
 
     Ok(())
 }
