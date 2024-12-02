3b275eb23c738981ff9eb652a2f78fadabdd89fd
RustyYato <rustyyato@gmail.com>
2024-12-02 12:13:19 -0700

    downgrade edition to 2021

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
+#![no_std]
+
 use rppal::gpio::Gpio;
 
 fn main() -> Result<(), rppal::gpio::Error> {
     let pins = Gpio::new()?;
     let pin2 = pins.get(2)?;
-    let led = pin2.into_output_low();
-
-    for _ in 0..10 {
-        //
-    }
 
     Ok(())
 }
